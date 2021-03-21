654621dcb726e774803373d19536a8c8a930c9c5
thomas-soares <thomas__soares@hotmail.com>
2021-03-21 04:41:32 -0300

    Modificando componente ExperienceBar.

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,8 +1,14 @@
 export function ExperienceBar() {
     return (
-        <header>
+        <header className="experience-bar">
             <span>0 xp</span>
-            <div />
+            <div>
+                <div style={{ width: '50%' }} />
+                
+                <span className="current-experience" style={{ left: '50%' }}>
+                    300 xp
+                </span>
+            </div>
             <span>600 xp</span>
         </header>
     );
